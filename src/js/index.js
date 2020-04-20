bc3c26fe45d1beddd181caefcf663ee3759f1bfb
DraqonFly <DraqonFly@github.com>
2020-04-20 22:02:58 +0200

    turn different figures into node-module

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import "../scss/index.scss";
-import { React } from "react";
+import {svg__blackRook, svg__blackBishop, svg__blackKing, svg__blackPawn, svg__blackQueen, svg__blackKnight, svg__whiteBishop, svg__whiteKing, svg__whiteKnight, svg__whitePawn, svg__whiteQueen, svg__whiteRook} from "./figures"
 
 console.log("hello world")
 
@@ -10,71 +10,29 @@ console.log(blackPawn)
 
 
 
-var svg__blackPawn = '<g> <path d="M 22,9 C 19.79,9 18,10.79 18,13 C 18,13.89 18.29,14.71 18.78,15.38 C 16.83,16.5 15.5,18.59 15.5,21 C 15.5,23.03 16.44,24.84 17.91,26.03 C 14.91,27.09 10.5,31.58 10.5,39.5 L 33.5,39.5 C 33.5,31.58 29.09,27.09 26.09,26.03 C 27.56,24.84 28.5,23.03 28.5,21 C 28.5,18.59 27.17,16.5 25.22,15.38 C 25.71,14.71 26,13.89 26,13 C 26,10.79 24.21,9 22,9 z " style="opacity:1; fill:#000000; fill-opacity:1; fill-rule:nonzero; stroke:#000000; stroke-width:1.5; stroke-linecap:round; stroke-linejoin:miter; stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;" /> </g>';
 
 
 window.createSVG = (svg) => {
     var tag;
     tag = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    document.body.appendChild(tag);
+    document.getElementById("figures").appendChild(tag);
     tag.innerHTML = svg;
     tag.classList.add("svgGenerated");
     tag = document.createElementNS("http://www.w3.org/2000/svg", "style");
-    document.body.appendChild(tag);
+    document.getElementById("figures").appendChild(tag);
     tag.innerHTML = "img.free {filter:url('#Matrix')}";
 }
 
+createSVG(svg__whiteRook)
+createSVG(svg__whiteBishop)
+createSVG(svg__whitePawn)
+createSVG(svg__whiteKing)
+createSVG(svg__whiteQueen)
+createSVG(svg__whiteKnight)
+
+createSVG(svg__blackRook)
+createSVG(svg__blackBishop)
 createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
-createSVG(svg__blackPawn)
+createSVG(svg__blackKing)
+createSVG(svg__blackQueen)
+createSVG(svg__blackKnight)
